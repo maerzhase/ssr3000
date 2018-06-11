ebec8edc7f0122d932f3b316532f46d6b1a183b8
maerzhase <markus@lili16.de>
2018-06-11 18:09:19 +0200

    move all context helpers to document

--- src/server/document.js
+++ src/server/document.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import SSR3000Script, { SSR3000Head, SSR3000Main } from './SSR3000Script';
+import { SSR3000Context } from './context';
+import { sortExternalsToTop } from '../webpack/utils';
 
-class Document extends Component {
+export default class Document extends Component {
   render() {
     return (
       <html>
@@ -17,5 +18,38 @@ class Document extends Component {
   }
 }
 
-export default Document;
+export const SSR3000Head = () => (
+  <SSR3000Context.Consumer>
+    {
+      SSR3000 => SSR3000.chunks.js
+        .sort(sortExternalsToTop)
+        .map(s => (
+          <script key={s} src={s}/>
+        )
+      )
+    }
+  </SSR3000Context.Consumer>
+);
 
+export const SSR3000Main = () => (
+  <SSR3000Context.Consumer>
+    {
+      SSR3000 => (
+        <div id="root">
+          <SSR3000.App/>
+        </div>
+      )
+    }
+  </SSR3000Context.Consumer>
+);
+
+export const SSR3000Script = () => (
+  <SSR3000Context.Consumer>
+    {
+      SSR3000 =>
+      <script dangerouslySetInnerHTML={{
+          __html: `window.__SSR3000.render.default(window.__SSR3000.${SSR3000.entry});`
+      }}/>
+    }
+  </SSR3000Context.Consumer>
+);
